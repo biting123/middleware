f228e340d33ab7740dca68c5e944f5a7f846f4b5
BiTing <101046872+biting123@users.noreply.github.com>
2022-03-20 21:46:29 +0800

    Add files via upload

---
+++ student/Test.java
@@ -0,0 +1,22 @@
+package student;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) throws Exception{
+        StudentDAo studentdao=new StudentDaoImpl();
+//        List<Student> list= studentdao.findAll();
+//        for(Student student:list){
+//            System.out.println("学号："+student.getId()+" 姓名："+student.getName()+" 性别："+student.getGender()+" 出生日期："+student.getBorndate()+" 班级："+student.getClassnum()+"\n");
+//        }//findAll()
+//        Student student=studentdao.findByStudentID((long) 201401001);
+//        System.out.println("学号："+student.getId()+" 姓名："+student.getName()+" 性别："+student.getGender()+" 出生日期："+student.getBorndate()+" 班级："+student.getClassnum()+"\n");
+           //findByStudentID
+//        Student students=studentdao.findByStudentID((long) 201401001);
+//        studentdao.deleteStudent(students);
+//        List<Student> list= studentdao.findAll();
+//        for(Student student:list){
+//            System.out.println("学号："+student.getId()+" 姓名："+student.getName()+" 性别："+student.getGender()+" 出生日期："+student.getBorndate()+" 班级："+student.getClassnum()+"\n");
+//        }//deleteStudent(students)
+    }
+}
